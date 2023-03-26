ad52fec9039167ddad50d2be6e04af742f99ecc7
nurealammiaji <nurealam.miaji@gmail.com>
2023-03-27 02:06:11 +0600

    event handler added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-apply').addEventListener('click', function(){
+    let realPrice = document.getElementById('real-price');
+    let realPriceValue = realPrice.innerText;
+    let realPriceValueNumeric = parseFloat(realPriceValue);
+    console.log(realPriceValueNumeric);
+
+    let discountPrice = document.getElementById('discount-price');
+    let discountPriceValue = discountPrice.innerText;
+    let discountPriceValueNumeric = parseFloat(discountPriceValue);
+    console.log(discountPriceValueNumeric);
+})
