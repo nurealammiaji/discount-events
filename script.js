650c9848e4168cec0ff172183152031065a48c7e
nurealammiaji <nurealam.miaji@gmail.com>
2023-03-27 02:20:33 +0600

    apply discount added

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ document.getElementById('btn-apply').addEventListener('click', function(){
     let realPrice = document.getElementById('real-price');
     let realPriceValue = realPrice.innerText;
     let realPriceValueNumeric = parseFloat(realPriceValue);
-    console.log(realPriceValueNumeric);
 
     let discountPrice = document.getElementById('discount-price');
     let discountPriceValue = discountPrice.innerText;
     let discountPriceValueNumeric = parseFloat(discountPriceValue);
-    console.log(discountPriceValueNumeric);
 
-    let applyDiscount = realPriceValueNumeric / 100;
-    let thirtyPercent = applyDiscount * 30;
-    discountPrice.innerText = thirtyPercent;
-    console.log(thirtyPercent.toFixed(2));
+    let applyDiscount = realPriceValueNumeric / 100 * 30;
+    let applyDiscountPrice = realPriceValueNumeric - applyDiscount;
+    discountPrice.innerText = applyDiscountPrice;
 })
